2a2dfee7322e8fde5a8e44b749e7e086804d20e8
Punpikorn Rattanawirojkul <punpikorn.r@gmail.com>
2021-05-24 14:15:12 +0700

    refactor: format

--- src/constants/HelList.ts
+++ src/constants/HelList.ts
@@ -1,43 +1,43 @@
-import { Substance, SubstanceGroup } from "../interfaces/Substance";
+import { Substance, SubstanceGroup } from '../interfaces/Substance';
 
 // alias should be lowercase
 
 const microplastic: Substance = {
-    key: "microplastic",
-    name: "Microplastic sphere or beads",
-    description: "",
+    key: 'microplastic',
+    name: 'Microplastic sphere or beads',
+    description: '',
     alias: [
-        "acrylates copolymer",
-        "acrylates crosspolymer",
-        "butylene",
-        "carbomer",
-        "dimethicone",
-        "ethylene",
-        "methacrylate copolymer",
-        "methacrylate crosspolymer",
-        "methyl methacrylate copolymer",
-        "methyl methacrylate crosspolymer",
-        "nylon",
-        "polyacrylamide",
-        "polyacrylate",
-        "polypropylene",
-        "polyurethane",
-        "polyvinyl",
-        "propylene copolymer or polypropylene",
-        "pvp",
-        "styrene copolymer",
-        "tetrafluoroethylene",
-        "vinyl acetate copolymer",
-        "vp/va copolymer",
+        'acrylates copolymer',
+        'acrylates crosspolymer',
+        'butylene',
+        'carbomer',
+        'dimethicone',
+        'ethylene',
+        'methacrylate copolymer',
+        'methacrylate crosspolymer',
+        'methyl methacrylate copolymer',
+        'methyl methacrylate crosspolymer',
+        'nylon',
+        'polyacrylamide',
+        'polyacrylate',
+        'polypropylene',
+        'polyurethane',
+        'polyvinyl',
+        'propylene copolymer or polypropylene',
+        'pvp',
+        'styrene copolymer',
+        'tetrafluoroethylene',
+        'vinyl acetate copolymer',
+        'vp/va copolymer'
     ],
-    urls: ["https://www.beatthemicrobead.org/guide-to-microplastics/"],
+    urls: ['https://www.beatthemicrobead.org/guide-to-microplastics/']
 };
 
 export const HelListGroup: SubstanceGroup = {
-    name: "HEL List",
+    name: 'HEL List',
     description:
-        "Harmful substances to environment (ex. coral and sea creatures)",
-    substances: [microplastic],
+        'Harmful substances to environment (ex. coral and sea creatures)',
+    substances: [microplastic]
 };
 
 // Any form of microplastic sphere or beads.
