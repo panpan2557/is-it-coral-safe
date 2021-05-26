3c100127e804a75bdbd2cbb6a96286376e118ec9
--global <punpikorn.rattanawirojkul@agoda.com>
2021-05-26 23:01:43 +0700

    feat: add result section

--- src/components/ValidationForm.tsx
+++ src/components/ValidationForm.tsx
@@ -1,25 +1,36 @@
 import React, { ChangeEvent, FormEvent, useState } from 'react';
+import { MatchSubstance } from '../interfaces/Substance';
 import { validate } from '../Utils/Validator';
 
 const ValidationForm = () => {
     const [text, setText] = useState<string>('');
+    const [isValidateClicked, setIsValidateClicked] = useState<boolean>(false);
+    const [matchSubstances, setMatchSubstances] = useState<MatchSubstance[]>(
+        []
+    );
     const onTextChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
         setText(event.target.value);
     };
-
     const handleSubmit = (e: FormEvent) => {
-        console.log('submit is clicked');
-        validate(text);
         e.preventDefault();
+        setMatchSubstances(validate(text));
+        setIsValidateClicked(true);
     };
+    const harmfulBox = <div>harmful</div>;
+    const safeBox = <div>safe</div>;
 
     return (
-        <form onSubmit={handleSubmit}>
-            <textarea value={text} onChange={onTextChange}/>
-            <div>
-                <button>Check</button>
+        <>
+            <form onSubmit={handleSubmit}>
+                <textarea value={text} onChange={onTextChange} />
+                <div>
+                    <button>Check</button>
+                </div>
+            </form>
+            <div className="match-result-container">
+                {isValidateClicked && (matchSubstances.length > 0 ? harmfulBox : safeBox)}
             </div>
-        </form>
+        </>
     );
 };
 
