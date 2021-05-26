a28d3f8b812dfbe1c295a0959d239ceb6f413b91
--global <punpikorn.rattanawirojkul@agoda.com>
2021-05-26 21:51:18 +0700

    feat: add more substances

--- src/constants/HelList.ts
+++ src/constants/HelList.ts
@@ -5,7 +5,7 @@ import { Substance, SubstanceGroup } from '../interfaces/Substance';
 const microplastic: Substance = {
     key: 'microplastic',
     name: 'Microplastic sphere or beads',
-    description: '',
+    description: 'test description for microplastic',
     alias: [
         'acrylates copolymer',
         'acrylates crosspolymer',
@@ -36,7 +36,7 @@ const microplastic: Substance = {
 const oxybenzone: Substance = {
     key: 'oxybenzone',
     name: 'Oxybenzone',
-    description: '',
+    description: 'test description for oxybenzone',
     alias: [
         'oxybenzone',
         'benzophenone-3',
@@ -47,19 +47,75 @@ const oxybenzone: Substance = {
     urls: [''],
 };
 
+const nanoparticles: Substance = {
+    key: 'nanoParticles',
+    name: 'Nanoparticles',
+    description: 'test description for nanoparticles',
+    alias: [
+        'fullerenes',
+        'micronized zinc oxide',
+        'nano zinc oxide',
+        'micronized titanium dioxide',
+        'micronized quartz silica',
+    ],
+    urls: [
+        'https://www.nationalgeographic.com/science/article/150514-sunscreen-nanoparticle-nanotechnology-oceans-marine-beach-boat-toxic',
+    ],
+};
+
+const octinoxate: Substance = {
+    key: 'oxtinoxate',
+    name: 'Octinoxate',
+    description: 'test description for octinoxate',
+    alias: [
+        'octinoxate',
+        'o methoxycinnamate (omc)',
+        'omc',
+        'parsol',
+        'parsol mcx',
+        'parsol mox',
+        'escalol',
+        '2-ethylhexyl p-methoxycinnamate',
+    ],
+    urls: [
+        'https://www.consumerreports.org/sunscreens/the-truth-about-reef-safe-sunscreen/',
+    ],
+};
+
+const octocrylene: Substance = {
+    key: 'octocrylene',
+    name: 'Octocrylene',
+    description: 'test descrription for octocrylene',
+    alias: [
+        'octocrylene',
+        'octocrilene',
+        'uvinul n-539',
+        '2-ethylhexyl 2-cyano-3',
+        '3-diphenylprop-2-enoate',
+    ],
+    urls: [],
+};
+
 export const HelListGroup: SubstanceGroup = {
     name: 'HEL List',
     description:
         'Harmful substances to environment (ex. coral and sea creatures)',
-    substances: [microplastic, oxybenzone],
+    substances: [
+        microplastic,
+        oxybenzone,
+        nanoparticles,
+        octinoxate,
+        octocrylene,
+    ],
 };
 
+// HEL List:
 // [DONE] Any form of microplastic sphere or beads.
-// Any nanoparticles like zinc oxide or titanium dioxide.
-// Oxybenzone
-// Octinoxate
+// [DONE] Any nanoparticles like zinc oxide or titanium dioxide.
+// [DONE] Oxybenzone
+// [DONE] Octinoxate
 // 4-methylbenzylidene camphor
-// Octocrylene
+// [DONE] Octocrylene
 // Para-aminobenzoic acid (PABA)
 // Methyl Paraben
 // Ethyl Paraben
